2af1990bca066501ec2adc9c0bc6131af60998ef
JeremieDrazic <jerem.drazic@gmail.com>
2016-04-25 23:44:25 +0200

    continued structure for movement detection

---
+++ src/helpers/sockets/socket.js
@@ -0,0 +1,67 @@
+import io from 'socket.io-client'
+
+export default class Socket {
+    constructor() {
+
+        // this.host = 'http://172.18.34.209:3000'
+        this.host = 'http://192.168.1.83:3000'
+    }
+
+    init(data) {
+        this.socket = io(this.host)
+
+        this.socket.on('newConnection', (data) => {
+            console.log('connected')
+            this.socket.emit('user-connected', data)
+        })
+
+        this.socket.on('disconnect', (data) => {
+            console.log('disconnected')
+            this.socket.emit('user-disconnected', data)
+        })
+    }
+
+    sendDeviceOrientation() {
+        window.addEventListener('deviceorientation', (event) => {
+            this.socket.emit('orientation', {
+                'alpha': event.alpha,
+                'beta': event.beta,
+                'gamma': event.gamma,
+                'timeStamp': event.orientationTs
+            })
+        })
+    }
+
+    sendDeviceMotion() {
+        window.addEventListener('devicemotion', (event) {
+            this.socket.emit('acceleration', {
+                'aX': event.acceleration.x,
+                'aY': event.acceleration.y,
+                'aZ': event.acceleration.z,
+
+                'aGravityX': event.accelerationIncludingGravity.x,
+                'aGravityY': event.accelerationIncludingGravity.y,
+                'aGravityZ': event.accelerationIncludingGravity.z,
+
+                'rRateX': event.rotationRate.beta,
+                'rRateY': event.rotationRate.gamma,
+                'rRateZ': event.rotationRate.alpha,
+
+                'interval': event.interval,
+                'timeStamp': event.accelerationTs
+            })
+        })
+    }
+
+    pauseGame() {
+        this.socket.emit('game-paused')
+    }
+
+    activateCoach() {
+        this.socket.emit('coach-acvtivated')
+    }
+
+    deactivateCoach() {
+        this.socket.emit('coach-deactivated')
+    }
+}
---
+++ src/helpers/user/userInfos.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "name": "Robin",
+    "slug": "Le meilleur des Drazic"
+    "sexe": "H",
+    "age": 23,
+    "progress": {
+        "nbWorldsDone": 2,
+        "nbWorldsLeft": 400,
+        "worlds": {
+            "water": {
+                "name": "Atlantis",
+                "score": 90000,
+                "percentLight": 30,
+                "nb": 1
+            },
+            "fire": {
+                "name": "Hell",
+                "score": 900000,
+                "percentLight": 50,
+                "nb": 2
+            }
+        }
+    },
+    "preferedAppSettings": {
+        "soundVolume": 50,
+        "inAppMusicVolume": 70,
+        "inAppSoundEffectsVolume": 80
+    }
+}
