36a5aa22ab0ca8eb4d8304ccb0d647740a64514f
DRAZIC Jeremie <jdrazic@p-212a-14.intra.gobelins.fr>
2016-05-02 17:07:42 +0200

    send data

--- src/helpers/sockets/socket.js
+++ src/helpers/sockets/socket.js
@@ -3,14 +3,22 @@ import io from 'socket.io-client'
 export default class Socket {
     constructor() {
 
-        this.host = 'http://172.18.34.209:3000'
+        this.host = 'http://172.18.33.150:3000'
         // this.host = 'http://192.168.1.83:3000'
 
         this.listening = false
         this.sending = false
+
+        this.currentXAccel = undefined
+        this.currentYAccel = undefined
+        this.currentZAccel = undefined
+
+        this.previousXAccel = undefined
+        this.previousYAccel = undefined
+        this.previousZAccel = undefined
     }
 
-    init(data) {
+    init() {
         this.socket = io(this.host)
         this.listening = true
 
@@ -38,11 +46,33 @@ export default class Socket {
     }
 
     sendDeviceMotion() {
-        window.addEventListener('devicemotion', (event) {
+        window.addEventListener('devicemotion', (event) => {
+
+            if(this.currentXAccel) {
+                this.previousXAccel = this.currentXAccel
+            }
+
+            if(this.currentYAccel) {
+                this.previousYAccel = this.currentYAccel
+            }
+
+            if(this.currentZAccel) {
+                this.previousZAccel = this.currentZAccel
+            }
+
+            this.currentXAccel = event.acceleration.x
+            this.currentYAccel = event.acceleration.y
+            this.currentZAccel = event.acceleration.z
+
             this.socket.emit('acceleration', {
-                'aX': event.acceleration.x,
-                'aY': event.acceleration.y,
-                'aZ': event.acceleration.z,
+
+                'aX': this.currentXAccel,
+                'aY': this.currentYAccel,
+                'aZ': this.currentZAccel,
+
+                'prevAX': this.previousXAccel,
+                'prevAY': this.previousYAccel,
+                'prevAZ': this.previousZAccel,
 
                 'aGravityX': event.accelerationIncludingGravity.x,
                 'aGravityY': event.accelerationIncludingGravity.y,
--- src/sections/home/home.js
+++ src/sections/home/home.js
@@ -2,6 +2,16 @@ import './home.scss'
 
 import Vue from 'vue'
 
+import SocketHandler from 'helpers/sockets/socket-handler'
+
 export default Vue.extend({
-    template: require('./home.html')
+    template: require('./home.html'),
+
+    ready() {
+        SocketHandler.init()
+
+        SocketHandler.sendDeviceMotion()
+
+        SocketHandler.sendDeviceOrientation()
+    }
 })
