3b0b87f64a8fe8374fe0a54bb38008f3ceef3781
JeremieDrazic <jerem.drazic@gmail.com>
2016-05-06 16:18:51 +0200

    merge

--- src/helpers/sockets/socket.js
+++ src/helpers/sockets/socket.js
@@ -3,8 +3,8 @@ import io from 'socket.io-client'
 export default class Socket {
     constructor() {
 
-        this.host = 'http://172.18.33.150:3000'
-        // this.host = 'http://192.168.1.83:3000'
+        // this.host = 'http://172.18.33.150:3000'
+        this.host = 'http://192.168.1.83:3000'
 
         this.listening = false
         this.sending = false
