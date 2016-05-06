167b7dd87fc96f381c1b052b877ec504e6ae223c
JeremieDrazic <jerem.drazic@gmail.com>
2016-05-06 22:56:40 +0200

    added components, sections and routes structures

--- src/sections/index.js
+++ src/sections/index.js
@@ -1,4 +1,11 @@
-import Home from './home/home.js'
-import Map from './map/map.js'
+import Home from './home/home'
+import Map from './worlds-map/worlds-map'
+import List from './worlds-list/worlds-list'
+import Detail from './worlds-detail/worlds-detail'
+import Credits from './credits/credits'
+import Profile from './profile/profile'
+import Remote from './remote/remote'
+import Results from './results/results'
+import Share from './share/share'
 
-export default { Home, Map }
+export default { Home, Map, List, Detail, Credits, Profile, Remote, Results, Share }
---
+++ src/sections/worlds-map/worlds-map.js
@@ -0,0 +1,7 @@
+import "./worlds-map.scss"
+
+import Vue from 'vue'
+
+export default Vue.extend({
+    template: require('./worlds-map.html')
+})
