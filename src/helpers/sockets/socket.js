11b971b821e20ef3be49a160da84a3df118013fc
JeremieDrazic <jerem.drazic@gmail.com>
2016-06-15 13:25:05 +0200

    blabla

--- src/helpers/sockets/socket.js
+++ src/helpers/sockets/socket.js
@@ -9,9 +9,8 @@ const REFERENCE_MESURE_RANGE   = 20
 export default class Socket {
     constructor() {
 
-      // this.host = 'http://172.18.33.23:3000'
-      // this.host = 'http://172.18.33.102:3000' //ecole
-      this.host = 'http://192.168.1.84:3000'  //appart
+      this.host = 'http://172.18.33.23:3000'
+    //   this.host = 'http://192.168.1.84:3000'  //appart
 
         this.motionReference    = {x: 0, y:0, z:0}
         this.rotationReference  = {alpha: 0, beta:0, gamma:0}
