b19bf2f663935fab31d0d66e1e1f1cb155592ca7
JeremieDrazic <jerem.drazic@gmail.com>
2016-06-17 01:44:35 +0200

    blabla

--- src/helpers/sockets/socket.js
+++ src/helpers/sockets/socket.js
@@ -10,8 +10,8 @@ export default class Socket {
     constructor() {
 
     //   this.host = 'http://172.18.33.23:3000'
-      this.host = 'http://172.18.33.95:3000'
-    //   this.host = 'http://192.168.1.84:3000'  //appart
+    //   this.host = 'http://172.18.33.95:3000'
+      this.host = 'http://192.168.1.84:3000'  //appart
 
         this.motionReference    = {x: 0, y:0, z:0}
         this.rotationReference  = {alpha: 0, beta:0, gamma:0}
