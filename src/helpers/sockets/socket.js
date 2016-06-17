554f3b79d484354ed3f06c2a6eac2e09a8cff068
JeremieDrazic <jerem.drazic@gmail.com>
2016-06-17 13:23:07 +0200

    changed scenario

--- src/helpers/sockets/socket.js
+++ src/helpers/sockets/socket.js
@@ -1,23 +1,25 @@
 import io from 'socket.io-client'
 
-const MAX_POOL_SIZE            = 2000
-const THRESHOLD_ACCELERATION   = 1200
-const THRESHOLD_ROTATION       = 20
+const MAX_POOL_SIZE = 2000
+const THRESHOLD_ACCELERATION = 1200
+const THRESHOLD_ROTATION = 20
 const THRESHOLD_ROTATION_STILL = 5
-const REFERENCE_MESURE_RANGE   = 20
+const REFERENCE_MESURE_RANGE = 20
 
 export default class Socket {
     constructor() {
 
-    //   this.host = 'http://172.18.33.23:3000'
-    //   this.host = 'http://172.18.33.95:3000'
-      this.host = 'http://192.168.1.84:3000'  //appart
+        //   this.host = 'http://172.18.33.23:3000'
+        this.host = 'http://172.18.33.95:3000'
+        //   this.host = 'http://192.168.1.84:3000'  //appart
 
-        this.motionReference    = {x: 0, y:0, z:0}
-        this.rotationReference  = {alpha: 0, beta:0, gamma:0}
+        //reference objects
+        this.motionReference = {x: 0, y: 0, z: 0}
+        this.rotationReference = {alpha: 0, beta: 0, gamma: 0}
 
-        this.motionRefPool      = []
-        this.rotationRefPool    = []
+        //pools to temporary store real time data
+        this.motionRefPool = []
+        this.rotationRefPool = []
 
         this.listening = false
     }
@@ -29,24 +31,27 @@ export default class Socket {
 
     handleMotion() {
         window.addEventListener('devicemotion', (event) => {
+            // On movement detection clean the data and subtract the reference (0 if not defined yet)
             let motionRefObject = {
                 x: Math.abs(Math.trunc(event.acceleration.x * 10000)) - this.motionReference.x,
                 y: Math.abs(Math.trunc(event.acceleration.y * 10000)) - this.motionReference.y,
                 z: Math.abs(Math.trunc(event.acceleration.z * 10000)) - this.motionReference.z
             }
+
+            //handle the motion pool as data flows
             this.motionRefPool.unshift(motionRefObject)
             this.motionRefPool.slice(MAX_POOL_SIZE, this.motionRefPool.length)
 
             if (!this.motionReference.x) {
                 if (this.motionRefPool.length >= REFERENCE_MESURE_RANGE) {
 
-                    let i = 0,
-                    notSoFar = true
-                    while(++i < REFERENCE_MESURE_RANGE && notSoFar) {
+                    //Use threshold constants to detect a reference position
+                    let i = 0, notSoFar = true
+                    while (++i < REFERENCE_MESURE_RANGE && notSoFar) {
                         let currentObj = this.motionRefPool[i]
-                        notSoFar = Math.abs(currentObj.x - motionRefObject.x) < THRESHOLD_ACCELERATION
-                            && Math.abs(currentObj.y - motionRefObject.y) < THRESHOLD_ACCELERATION
-                            && Math.abs(currentObj.z - motionRefObject.z) < THRESHOLD_ACCELERATION
+                        notSoFar = Math.abs(currentObj.x - motionRefObject.x) < THRESHOLD_ACCELERATION &&
+                            Math.abs(currentObj.y - motionRefObject.y) < THRESHOLD_ACCELERATION &&
+                            Math.abs(currentObj.z - motionRefObject.z) < THRESHOLD_ACCELERATION
                     }
 
                     if (notSoFar) {
@@ -61,6 +66,7 @@ export default class Socket {
                     z: motionRefObject.z - this.motionRefPool[currentMotionPoolLength - 2].z
                 }
 
+                //Emit necessary events
                 this.socket.emit('motion', motionRefObject)
                 this.socket.emit('ref-motion', this.motionReference)
                 this.socket.emit('delta-motion', deltaMovementValue)
@@ -68,6 +74,7 @@ export default class Socket {
         })
     }
 
+    //Same method for rotation
     handleRotation() {
         window.addEventListener('deviceorientation', (event) => {
 
@@ -84,13 +91,13 @@ export default class Socket {
                 if (this.rotationRefPool.length >= REFERENCE_MESURE_RANGE) {
 
                     let j = 0,
-                    notTooFar = true
-                    while(++j < REFERENCE_MESURE_RANGE && notTooFar) {
+                        notTooFar = true
+                    while (++j < REFERENCE_MESURE_RANGE && notTooFar) {
                         let currentObj = this.rotationRefPool[j]
-                        notTooFar = (rotationRefObject.beta > 90 - THRESHOLD_ROTATION && rotationRefObject.beta < 90 + THRESHOLD_ROTATION)
-                            && (rotationRefObject.alpha > 200 - THRESHOLD_ROTATION && rotationRefObject.alpha < 200 + THRESHOLD_ROTATION)
-                            && (rotationRefObject.gamma > 50 - THRESHOLD_ROTATION && rotationRefObject.gamma < 50 + THRESHOLD_ROTATION)
-                            && (Math.abs(currentObj.beta - rotationRefObject.beta) < THRESHOLD_ROTATION_STILL)
+                        notTooFar = (rotationRefObject.beta > 90 - THRESHOLD_ROTATION && rotationRefObject.beta < 90 + THRESHOLD_ROTATION) &&
+                            (rotationRefObject.alpha > 200 - THRESHOLD_ROTATION && rotationRefObject.alpha < 200 + THRESHOLD_ROTATION) &&
+                            (rotationRefObject.gamma > 50 - THRESHOLD_ROTATION && rotationRefObject.gamma < 50 + THRESHOLD_ROTATION) &&
+                            (Math.abs(currentObj.beta - rotationRefObject.beta) < THRESHOLD_ROTATION_STILL)
                     }
 
                     if (notTooFar) {
